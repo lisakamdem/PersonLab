843bdd0105b6380bdc6f4fb3f61fcf02d495af34
lisakamdem <120063552+lisakamdem@users.noreply.github.com>
2023-01-25 14:23:36 -0700

    School Lab

---
+++ Person.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Person
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColor;
+        private bool isWorking;
+
+        //have get/set for the private 
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }  
+            set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }   
+            set { this.lastName = value; }  
+        }
+
+        public int Age
+        {
+            get { return this.age; }    
+            set { this.age = value; }
+        }
+
+        public string FavoriteColor
+        {
+            get { return this.favoriteColor; }
+            set { this.favoriteColor = value; }
+        }
+
+        public bool IsWorking
+        {
+            get
+            {
+                /*
+                string working;
+
+                if (this.isWorking == true)
+                {
+                    working = "True";
+                }
+                else
+                {
+                    working = "False";
+                }
+
+                return working;
+                */
+                return this.isWorking;
+            }
+            set { this.isWorking = value; }
+        }
+
+        public Person(int id, string firstName, string lastName, string favoriteColor, int age, bool isWorking) 
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.favoriteColor = favoriteColor;
+            this.isWorking = isWorking;
+        } 
+
+        public override string ToString() 
+        {
+            string format = "";
+
+            format += "ID: " + id + "\n";
+            format += "First Name: " + firstName + "\n";
+            format += "Last Name: " + lastName + "\n";
+            format += "Age: " + age + "\n";
+            format += "Favorite color: " + favoriteColor + "\n";
+            format += "Is working: " + isWorking + "\n";
+            return format;
+        }
+
+        public string ChangeFavoriteColor()
+        {
+            return favoriteColor = "white";
+        }
+
+        public string DisplayPersonInfo()
+        {
+            string name;
+            name = firstName + " " +lastName;
+            return name;
+        }
+
+        public int GetAgeInTenYears()
+        {
+            return age + 10;
+        }
+    }
+}
---
+++ PersonMain.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class PersonMain
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1,"Ian", "Brooks", "Red", 30, true);
+            Person person2 = new Person(2,"Gina","James", "Green", 18, false);
+            Person person3 = new Person(3, "Mike", "Briscoe", "blue", 45, true);
+            Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            
+
+            //Display's Gina info as a sentence that shows her id, name, and favorite color
+            Console.WriteLine(person2.FirstName + " " + person2.LastName + " her id is: " + person2.Id + " her favorite color is: " + person2.FavoriteColor);
+
+            //display all of Mike's info as a list
+            Console.WriteLine(person3.ToString());
+
+            //change Ian's favorite color to white, than display it in a sentence
+            person1.ChangeFavoriteColor();
+            Console.WriteLine(person1.FirstName + " " + person1.LastName + " id is: " + person1.Id + " his favorite color is: " + person1.FavoriteColor);
+
+            //Display Mary's age after 10 years
+            Console.WriteLine( person4.DisplayPersonInfo() + "'s age in 10 years is going to be: " + person4.GetAgeInTenYears());
+
+           List<Person> people= new List<Person>();
+
+            people.Add(person1);    
+            people.Add(person2);
+            people.Add(person3);
+            people.Add(person4);
+
+            //It works if I do it all in one for loop
+            //However it doesn't print it in the order I want it to
+            double age = 0;
+            double ageAverage;
+            for (int i = 0; i < people.Count; i++)
+            {
+                age += people[i].Age;
+            }
+
+            ageAverage = age / people.Count;
+            Console.WriteLine("The average age is: " + ageAverage);
+
+            int max = people[0].Age;
+            int min = people[0].Age;
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (people[i].Age > max)
+                {
+                    max = people[i].Age;
+                    string maxName = people[i].DisplayPersonInfo();
+                    Console.WriteLine("The Oldest person is " + maxName + " and is " + max + " years old");
+                }
+
+                if (people[i].Age < min)
+                {
+                    min = people[i].Age;
+                    string minName = people[i].DisplayPersonInfo();
+                    Console.WriteLine("The youngest age is " + minName + " and is " + min + " years old");
+                }
+            }
+
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                string name = people[i].FirstName;
+                char nameChar = name[0];
+
+                if (nameChar == 'M')
+                {
+                    Console.WriteLine("First letter of their name start with a 'M' is " + people[i].DisplayPersonInfo());
+                }
+
+            }
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                string color = people[i].FavoriteColor;
+                if (color == "blue")
+                {
+                    Console.WriteLine("The people who like the color blue are: \n" + people[i].ToString() );
+                }
+
+            }
+
+        }
+    }
+}
